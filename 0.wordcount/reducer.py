d8727ad91192dfa7f436e63ab708df1f2dbe3719
hyojin <dsvit100hjin@gmail.com>
2025-04-08 17:20:54 +0900

    update

--- 0.wordcount/reducer.py
+++ 0.wordcount/reducer.py
@@ -16,12 +16,13 @@ for line in sys.stdin:
     word, value = line.split('\t') # \tap 기준으로 자르는데, 첫번째 데이터는 word에 두번째 데이터는 value에 넣어줘
     value = int(value) # sys.stdin은 숫자처럼 보여도 모두 str으로 생각함, 따라서 int로 감싸주기
 
-    if last_word == word:
+    if last_word == word: # 만약 last_word가 내가 보고 있는 현재 word와 같다면 / 1회차 last_word는 None, 따라서 첫번째는 else가 실행됨
         total_count += value
     else:
         if last_word is not None:
             print(f'{last_word}\t{total_count}')
-        last_word = word # last_word가 끝나면 현재 워드로 바꿔줌
+        last_word = word # 첫번째 반복인 경우 진행되는 코드 - last_word(None) 가 last_word(apple)로 바뀜, 다시 for문 돌림
+        # word값이 바뀌면 값을 초기화해줌
         total_count = value # 다른 워드로 바뀌었기 때문에 값을 바꿔주는 것
 
 if last_word == word:
